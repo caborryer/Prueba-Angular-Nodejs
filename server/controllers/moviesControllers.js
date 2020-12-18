fdbf0a28844281fdeeb6ba9d1f62ebfc03010801
caborryer <ycorrale@eafit.edu.co>
2020-12-18 16:38:36 -0500

    routes and controllers are updated

--- server/controllers/moviesControllers.js
+++ server/controllers/moviesControllers.js
@@ -11,22 +11,27 @@ class moviesController {
 
     if (movies.data) {
       res.json(movies.data.results)
-      console.log(movies.data.results)
+    }
+  };
+
+  async getresultfromSearch(req, res) {
+    const {term} = req.params
+    const movies = await searchMovie(term)
+    if (movies.data) {
+      res.json(movies.data.results)
     }
 
+  };
+  async getresultFromNowmovies(req, res) {
+    const movies = await getTheatresMovies()
+
+    if (movies.data) {
+      res.json(movies.data.results)
+    }
   };
 
 }
 
-/*onst countBreeds = async () => {
-  const movies = await getTopMovies()
 
-  if (movies.data) {
-    res.json(movies.data.results)
-    console.log(movies.data.results)
-  }
-}
-
-countBreeds()c*/
 
 module.exports = moviesController;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,47 +1,13 @@
-const router = require("express").Router();
-const {getTopMovies, searchMovie, getTheatresMovies} = require('../models/callingData')
+const router = require('express').Router();
 const moviesController = require('../controllers/moviesControllers');
 
 const movie = new moviesController();
 
 router.route('/topMovies').get(movie.getresultTopMovies);
-
-router.get("/search:term", (req, res, next) => {
-
-  const countBreeds = async () => {
-   const {term} = req.params
-    console.log(term)
-
-    const movies = await searchMovie(term)
-    if (movies.data) {
-      res.json(movies.data.results)
-      console.log(movies.data.results)
-    }
-
-  }
-
-  countBreeds()
+router.route('/search:term').get(movie.getresultfromSearch);
+router.route('/nowMovies').get(movie.getresultFromNowmovies);
 
 
-
-});
-
-router.get("/now", (req, res, next) => {
-
-  const countBreeds = async () => {
-    const movies = await getTheatresMovies()
-
-    if (movies.data) {
-      res.json(movies.data.results)
-      console.log(movies.data.results)
-    }
-  }
-
-  countBreeds()
-
-
-
-});
 
 module.exports = router;
 
