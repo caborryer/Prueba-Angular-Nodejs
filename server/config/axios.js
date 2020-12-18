d4d5c1af750f4406d93be672048bb5f3a9b746ff
caborryer <ycorrale@eafit.edu.co>
2020-12-18 15:54:32 -0500

    fixed some issues

--- server/config/axios.js
+++
@@ -1,6 +0,0 @@
-const axios = require("axios");
-
-const axiosFetch = axios.create({
-  baseURL: "https://api.themoviedb.org/3/"
-});
-module.exports = axiosFetch
--- server/index.js
+++ server/index.js
@@ -12,19 +12,3 @@ app.listen(app.get('port'), () => {
 });
 
 
-//const app = require("express")();
-//const bodyParser = require("body-parser");
-//const cors = require("cors");
-//const routes = require("./routes/routes");
-
-//app.set('port', 2000);
-
-
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json({ limit: "10mb" }));
-
-//app.use('/', routes);
-
-//app.listen(app.get('port'), () => {
-  //console.log(`///server on port`, app.get(`port`))
-//});
