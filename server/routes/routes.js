84372f9f2e2dc5421d5571893331d53c148ddab8
caborryer <ycorrale@eafit.edu.co>
2020-12-18 13:05:40 -0500

    first service is working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,18 +1,28 @@
 const router = require("express").Router();
-const axiosFetch = require("../config/axios")
+const axios = require('axios')
 
 router.get("/test", (req, res, next) => {
 
-  async function getAll() {
+  const getTopMovies = async () => {
     try {
-      const results = await axiosFetch.get(`discover/movie?sort_by=popularity.desc&api_key=38e73fff75a56ceab612b735990996a2`)
-
-      return results.data
-
+      return await axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=38e73fff75a56ceab612b735990996a2&language=en-US&page=1')
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
   }
+
+  const countBreeds = async () => {
+    const movies = await getTopMovies()
+
+    if (movies.data) {
+      res.json(movies.data.results)
+      console.log(movies.data.results)
+    }
+  }
+
+  countBreeds()
+
+
 
 });
 
